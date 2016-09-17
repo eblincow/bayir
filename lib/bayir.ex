5a4fb67ae0688c3dd131295a2e5c9fcabc7ca378
Eric Blincow <eblincow@barracuda.com>
2016-09-17 00:06:45 -0400

    YAY working ascii camels

--- lib/bayir.ex
+++ lib/bayir.ex
@@ -1,6 +1,7 @@
 defmodule Bayir do
+   
   def main(args) do
-    IO.puts "Hello snorld!"
+    IO.puts "بَعير طابعة - reversed?!Hello snorld!"
     args |> parse_args |> process 
   end
 
@@ -9,19 +10,28 @@ defmodule Bayir do
     IO.puts "No arguments zgiven!"
   end
 
-  def process(options) do
-    {:ok, art} = File.read "ascii.art" 
+  def process(options) do 
+    if options[:times] do  
+      {:ok, art} = File.read "ascii.art" 
+      case options[:times] do
+        0 -> IO.puts "No camels, please!"
+        1 -> print_ascii(art, options[:name]) # one lonely camel!
+        x when x > 1 -> 
+          for n <- 1..options[:times], do: 
+            print_ascii(art, options[:name]) 
+      end
+    end
+  end
+       
+  def print_ascii(art,name) do
     IO.puts(art)
+    IO.puts "Hello #{name}!!" 
   end
 
   defp parse_args(args) do
     {options, _,_} = OptionParser.parse(args,
-      switches: [name: :string]
+      switches: [name: :string, times: :integer]
       )
     options
   end
-
 end
-
-
-
