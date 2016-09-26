86c8dd2214a62082c064871b145cd56b28b484f7
Eric Blincow <eblincow@barracuda.com>
2016-09-26 01:50:51 -0400

    Added new ascii art, new options

--- lib/bayir.ex
+++ lib/bayir.ex
@@ -1,23 +1,30 @@
 defmodule Bayir do
    
   def main(args) do
-    IO.puts "بَعير طابعة - reversed?!Hello snorld!"
+    IO.puts "بَعير طابعة - !Hello snorld!"
     args |> parse_args |> process 
   end
 
 
   def process([]) do
-    IO.puts "No arguments zgiven!"
+    IO.puts "No arguments given!"
   end
 
   def process(options) do 
-    if options[:times] do  
-      {:ok, art} = File.read "ascii.art" 
+    if options[:times] && options[:kind] do  
+    {_, art} =  
+      case options[:kind] do
+        "camel" -> File.read "ascii_art/camel1.ascii" 
+        "parrot" -> File.read "ascii_art/parrot.ascii"
+        "snoopy" -> File.read "ascii_art/snoopy.ascii" 
+        "bunny" -> File.read "ascii_art/bunny.ascii" 
+        _ -> {:ok,"AAAAAAAAAAAAAAAABBBBBBBBBAAAAAA"}
+      end 
       case options[:times] do
-        0 -> IO.puts "No camels, please!"
+        0 -> IO.puts "No times, whaa?"
         1 -> print_ascii(art, options[:name]) # one lonely camel!
         x when x > 1 -> 
-          for n <- 1..options[:times], do: 
+          for _ <- 1..options[:times], do: 
             print_ascii(art, options[:name]) 
       end
     end
@@ -25,7 +32,7 @@ defmodule Bayir do
        
   def print_ascii(art,name) do
     IO.puts(art)
-    IO.puts "Hello #{name}!!" 
+    IO.puts "#{name}!!" 
   end
 
   defp parse_args(args) do
